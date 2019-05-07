935acf4fca719757a4ae1df497f3ab38a4e236fa
Lucas Lacerda <lucaaslb@hotmail.com>
2019-05-06 22:14:32 -0300

    arquivo de treino, modelo treinado log e grafico de saida

--- lenet5_mnist.py
+++ lenet5_mnist.py
@@ -1,40 +1,42 @@
 """
 Treina uma CNN com o dataset MNIST.
  
-A CNN é inspirada na arquitetura LeNet-5, com algumas
-alterações nas funções de ativação, padding e pooling.
-"""
- 
+A CNN é inspirada na arquitetura LeNet-5
+""" 
 # importar pacotes necessários
 from keras.utils import to_categorical
 from keras.optimizers import SGD
 from keras import backend
 from keras.datasets import mnist
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 import numpy as np
 import h5py
+import time
 from cnn import LeNet5     #classe criada na pasta cnn
 
 # importar e normalizar o dataset MNIST
 # input image dimensions
-img_rows, img_cols = 28, 28
+imgX, imgY = 28, 28
+EPOCHS = 20
 
-print('[INFO] Download dataset')
+print('[INFO] Download dataset: MNIST')
+
 # dividir o dataset entre train (60000) e test (10000)
 (trainX, trainY), (testX, testY) =  mnist.load_data() 
 
 
-print('[INFO] Padronizando imagens de acordo com a lib utilizada em backend pelo Keras (TensorFlow ou Theano)')
+print('[INFO] Padronizando imagens de acordo com a lib utilizada em backend pelo Keras')
 if backend.image_data_format() == "channels_last": #Tensorflow backend
-    trainX = trainX.reshape(trainX.shape[0], img_rows, img_cols, 1)
-    testX = testX.reshape(testX.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    print('[INFO] Tensorflow')
+    trainX = trainX.reshape(trainX.shape[0], imgX, imgY, 1)
+    testX = testX.reshape(testX.shape[0], imgX, imgY, 1)
+    input_shape = (imgX, imgY, 1)
 else: #Theano backend
-    trainX = trainX.reshape(trainX.shape[0], 1, img_rows, img_cols)
-    testX = testX.reshape(testX.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+    print('[INFO] Theano')
+    trainX = trainX.reshape(trainX.shape[0], 1, imgX, imgY)
+    testX = testX.reshape(testX.shape[0], 1, imgX, imgY)
+    input_shape = (1, imgX, imgY)
 
 trainX = trainX.astype('float32')
 testX = testX.astype('float32')
@@ -47,32 +49,43 @@ testY = to_categorical(testY, 10)
 
 # inicializar e otimizar modelo
 print("[INFO] inicializando e otimizando a CNN...")
+begin = time.time()
+
 model = LeNet5.build(28, 28, 1, 10)
 model.compile(optimizer=SGD(0.01), loss="categorical_crossentropy",
               metrics=["accuracy"])
  
 # treinar a CNN
 print("[INFO] treinando a CNN...")
-H = model.fit(trainX, trainY, batch_size=128, epochs=20, verbose=2,
+H = model.fit(trainX, trainY, batch_size=128, epochs=EPOCHS, verbose=1,
           validation_data=(testX, testY))
 
-model.save('models/trained_model.h5')
+print("[INFO] Salvando modelo treinado ...")
+model.save('models/trained_model_mnist.h5')
+
+end = time.time()
+
+print("[INFO] tempo de execução da CNN: %.2f s" %(end - begin))
 
 # avaliar a CNN
 print("[INFO] avaliando a CNN...")
 predictions = model.predict(testX, batch_size=64)
 print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1),
                             target_names=[str(label) for label in range(10)]))
+score = model.evaluate(testX, testY, verbose=1)
+print('[INFO] Test Loss: ', score[0])
+print('[INFO] Test Accuracy: ', score[1])
 
+print("[INFO] Plot loss e accuracy para os datasets 'train' e 'test'")
 # plotar loss e accuracy para os datasets 'train' e 'test'
 plt.style.use("ggplot")
 plt.figure()
-plt.plot(np.arange(0,20), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0,20), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0,20), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0,20), H.history["val_acc"], label="val_acc")
+plt.plot(np.arange(0,EPOCHS), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0,EPOCHS), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0,EPOCHS), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0,EPOCHS), H.history["val_acc"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend()
-plt.savefig('cnn.png', bbox_inches='tight')
+plt.savefig('models/cnn_mnist.png', bbox_inches='tight')
